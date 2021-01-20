4ee4207a8a97b92a80e9efae2d0bbf635d033079
xxKR15Hxx <krishtango198@gmail.com>
2021-01-20 18:04:56 +0530

    first commit

---
+++ Desktop/WhitehatSr/AngryBirdsStage2.5StudentActivity-master/Chain.js
@@ -0,0 +1,18 @@
+class Chain {
+    constructor(bodyA, bodyB) {
+        var options = {
+            bodyA: bodyA,
+            bodyB: bodyB,
+            stiffness: 0.04,
+            length: 20
+        }
+        this.chain = Constraint.create(options);
+        World.add(world, chain)
+    }
+    display() {
+        var pointA = this.chain.bodyA.position;
+        var pointB = this.chain.bodyB.position;
+        strokeWeight(3);
+        line(pointA.x, pointA.y, pointB.x, pointB.y);
+    }
+}
